0ee48d8aed45f7389e4249e75d3ae2e4cd1a01b4
Frank <frank.leuchtmann@gmx.de>
2018-11-08 05:10:11 +0100

    init commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        System.setProperty("webdriver.gecko.driver", "src/test/resources/geckodriver");
+
+        WebDriver driver = new FirefoxDriver();
+
+        driver.get("https://www.youtube.com/watch?v=JGHWVAN_yQg");
+    }
+}
