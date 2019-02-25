4ca8404d8587fb718d1da377a607397e3d7d8a8a
Frank <frank.leuchtmann@gmx.de>
2019-02-25 20:17:55 +0100

    made work again

--- src/main/java/Second.java
+++ src/main/java/Second.java
@@ -1,17 +1,14 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+public class Second {
 
-
-public class Main {
-
-    public static void main(String[] args) {
-
-
+    public static void main(String[] args){
         System.setProperty("webdriver.gecko.driver", "src/test/resources/geckodriver");
 
         WebDriver driver = new FirefoxDriver();
 
         driver.get("https://www.youtube.com/watch?v=JGHWVAN_yQg");
+
     }
 }
